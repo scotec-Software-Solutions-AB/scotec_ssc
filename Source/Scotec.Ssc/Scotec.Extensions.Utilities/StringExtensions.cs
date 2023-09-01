c217e1d9e950dd0995330382a86a5bb29e0d5dbe
Olaf Meyer <olaf.meyer@e-ypsilon.de>
2023-09-01 13:00:02 +0200

    Added utility project and string extensions.

---
+++ Source/Scotec.Ssc/Scotec.Extensions.Utilities/StringExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq.Expressions;
+
+namespace Scotec.Extensions.Utilities;
+
+public static class StringExtensions
+{
+#if NETSTANDARD2_1_OR_GREATER
+    public static string Format(this string template, params Expression<Func<string, object>>[] args)
+    {
+        return template.Format(StringComparison.Ordinal, args);
+    }
+#endif
+
+    public static string Format(this string template
+#if NETSTANDARD2_1_OR_GREATER
+                                , StringComparison comparison
+#endif
+                                , params Expression<Func<string, object>>[] args)
+    {
+        if (args == null)
+        {
+            throw new ArgumentNullException(nameof(args));
+        }
+
+        if (string.IsNullOrEmpty(template))
+        {
+            return string.Empty;
+        }
+
+        var parameters = args.Select(e => (Key: $"{{{e.Parameters[0].Name}}}", Value: e.Compile()(e.Parameters[0].Name!)));
+
+        return parameters.Aggregate(template, (a, b) => a.Replace(b.Key, b.Value?.ToString() ?? string.Empty
+#if NETSTANDARD2_1_OR_GREATER
+            , comparison
+#endif
+        ));
+    }
+}
---
+++ Source/Scotec.Ssc/Tests/Scotec.Extensions.Utilities.Tests/StringFormatTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace Scotec.Extensions.Utilities.Tests;
+
+public class StringFormatTest
+{
+    [Theory]
+    [InlineData("_{value}_", StringComparison.Ordinal, "A", "_A_")]
+    [InlineData("_{Value}_", StringComparison.Ordinal, "A", "_{Value}_")]
+    [InlineData("_{Value}_", StringComparison.OrdinalIgnoreCase, "A", "_A_")]
+    public void Test(string template, StringComparison comparison, string data, string expected)
+    {
+        var x = template.FormatX( () => );
+
+
+        var result = template.Format(comparison, value => data);
+        Assert.Equal(expected, result);
+    }
+}
