95fa4c4e425525382f53aca0f32199cd34af67b7
Olaf Meyer <olaf.meyer@e-ypsilon.de>
2023-09-27 04:01:28 +0200

    Added test for image server

---
+++ Source/Scotec.Ssc/Tests/Scotec.Web.ImageServer.Test/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Scotec.Web.ImageServer.Test;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        var config = new ConfigurationBuilder()
+                     .SetBasePath(AppContext.BaseDirectory)
+                     .AddJsonFile("appsettings.json", false, true)
+                     .Build();
+        services.AddSingleton<IConfiguration>(config);
+
+        services.AddImageServer()
+                .AddLocalImageProvider("images")
+                .AddAzureBlobStorageImageProvider("scotecblog");
+    }
+}
