6d0c0bcce1e99bf35b2eff1619a196767652e8bf
Olaf Meyer <olaf.meyer@e-ypsilon.d>
2024-12-26 18:27:11 +0100

    Added Enum-String Attriubute. Added enum extensions for converting enums to strings. Added JSON enum/string converter.

---
+++ Source/Scotec.Ssc/Scotec.Extensions.Utilities/EnumExtensions.cs
@@ -0,0 +1,80 @@
+﻿using System.Reflection;
+
+namespace Scotec.Extensions.Utilities;
+
+/// <summary>
+/// Provides extension methods for working with enums, including converting between enums and their associated string values.
+/// </summary>
+public static class EnumExtensions
+{
+    /// <summary>
+    ///     Converts an enum field to its associated string value.
+    /// </summary>
+    /// <typeparam name="TEnum">The type of the enum.</typeparam>
+    /// <param name="enumValue">The enum value to convert.</param>
+    /// <returns>The associated string value, or null if no attribute is found.</returns>
+    public static string ToStringValue<TEnum>(this TEnum enumValue)
+        where TEnum : Enum
+    {
+        var stringValue = enumValue.ToString();
+        var fieldInfo = typeof(TEnum).GetField(stringValue);
+        var attribute = fieldInfo?.GetCustomAttribute<EnumStringValueAttribute>();
+
+        return attribute?.StringValue ?? stringValue;
+    }
+
+    /// <summary>
+    ///     Converts a string value to its corresponding enum field.
+    /// </summary>
+    /// <typeparam name="TEnum">The type of the enum.</typeparam>
+    /// <param name="stringValue">The string value to convert.</param>
+    /// <returns>
+    ///     The corresponding enum field of type <typeparamref name="TEnum" />.
+    /// </returns>
+    /// <exception cref="ArgumentException">
+    ///     Thrown when no enum field with the specified string value is found in the enum type.
+    /// </exception>
+    public static TEnum ToEnumValue<TEnum>(this string stringValue)
+        where TEnum : Enum
+    {
+        return (TEnum)ToEnumValue(stringValue, typeof(TEnum));
+    }
+
+    /// <summary>
+    ///     Converts a string value to its corresponding enum field.
+    /// </summary>
+    /// <param name="stringValue">The string value to convert.</param>
+    /// <param name="enumType">The type of the enum.</param>
+    /// <returns>
+    ///     The corresponding enum field as an <see cref="Enum" />.
+    /// </returns>
+    /// <exception cref="ArgumentException">
+    ///     Thrown when no enum field with the specified string value is found in the specified enum type.
+    /// </exception>
+    /// <exception cref="ArgumentNullException">
+    ///     Thrown when <paramref name="stringValue" /> or <paramref name="enumType" /> is <c>null</c>.
+    /// </exception>
+    public static Enum ToEnumValue(this string stringValue, Type enumType)
+    {
+        var fields = enumType.GetFields(BindingFlags.Public | BindingFlags.Static);
+        foreach (var field in fields)
+        {
+            var attribute = field.GetCustomAttribute<EnumStringValueAttribute>();
+            if (attribute?.StringValue == stringValue)
+            {
+                return (Enum)field.GetValue(null)!;
+            }
+        }
+
+#if NETSTANDARD2_1_OR_GREATER
+        if (Enum.TryParse(enumType, stringValue, out var value))
+        {
+            return (Enum)value;
+        }
+#else
+        return (Enum)Enum.Parse(enumType, stringValue);
+
+#endif
+        throw new ArgumentException($"No enum field with string value '{stringValue}' found in {enumType.Name}.");
+    }
+}
---
+++ Source/Scotec.Ssc/Scotec.Extensions.Utilities/EnumStringValueAttribute.cs
@@ -0,0 +1,22 @@
+﻿namespace Scotec.Extensions.Utilities;
+
+/// <summary>
+/// An attribute used to associate a string value with an enum field.
+/// </summary>
+[AttributeUsage(AttributeTargets.Field)]
+public class EnumStringValueAttribute : Attribute
+{
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="EnumStringValueAttribute" /> class.
+    /// </summary>
+    /// <param name="stringValue">The string value to associate with the enum field.</param>
+    public EnumStringValueAttribute(string stringValue)
+    {
+        StringValue = stringValue;
+    }
+
+    /// <summary>
+    /// Gets the string value associated with the enum field.
+    /// </summary>
+    public string StringValue { get; }
+}
