38064a7286d1c3fd6448132876ff13ade670bc15
Olaf Meyer <olaf.meyer@e-ypsilon.d>
2024-12-26 18:28:20 +0100

    Code cleanup.

--- Source/Scotec.Ssc/Scotec.Extensions.Utilities/EnumStringValueJsonConverter.cs
+++ Source/Scotec.Ssc/Scotec.Extensions.Utilities/EnumStringValueJsonConverter.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection;
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Scotec.Extensions.Utilities;
@@ -12,13 +11,13 @@ public class EnumStringValueJsonConverter : JsonConverter<Enum>
     }
 
     /// <summary>
-    /// Writes the JSON representation of the specified <see cref="Enum"/> value.
+    ///     Writes the JSON representation of the specified <see cref="Enum" /> value.
     /// </summary>
-    /// <param name="writer">The <see cref="Utf8JsonWriter"/> to write to.</param>
-    /// <param name="value">The <see cref="Enum"/> value to write.</param>
-    /// <param name="options">The <see cref="JsonSerializerOptions"/> to use during serialization.</param>
+    /// <param name="writer">The <see cref="Utf8JsonWriter" /> to write to.</param>
+    /// <param name="value">The <see cref="Enum" /> value to write.</param>
+    /// <param name="options">The <see cref="JsonSerializerOptions" /> to use during serialization.</param>
     /// <exception cref="ArgumentNullException">
-    /// Thrown when the <paramref name="writer"/> or <paramref name="value"/> is <c>null</c>.
+    ///     Thrown when the <paramref name="writer" /> or <paramref name="value" /> is <c>null</c>.
     /// </exception>
     public override void Write(Utf8JsonWriter writer, Enum value, JsonSerializerOptions options)
     {
@@ -26,17 +25,17 @@ public class EnumStringValueJsonConverter : JsonConverter<Enum>
     }
 
     /// <summary>
-    /// Reads and converts the JSON representation of an enum value to its corresponding <see cref="Enum"/> object.
+    ///     Reads and converts the JSON representation of an enum value to its corresponding <see cref="Enum" /> object.
     /// </summary>
-    /// <param name="reader">The <see cref="Utf8JsonReader"/> to read from.</param>
+    /// <param name="reader">The <see cref="Utf8JsonReader" /> to read from.</param>
     /// <param name="typeToConvert">The type of the enum to convert to.</param>
-    /// <param name="options">The <see cref="JsonSerializerOptions"/> to use during deserialization.</param>
+    /// <param name="options">The <see cref="JsonSerializerOptions" /> to use during deserialization.</param>
     /// <returns>The enum value corresponding to the string representation in the JSON.</returns>
     /// <exception cref="ArgumentNullException">
-    /// Thrown when the JSON string value is <c>null</c>.
+    ///     Thrown when the JSON string value is <c>null</c>.
     /// </exception>
     /// <exception cref="ArgumentException">
-    /// Thrown when the string value does not match any field in the specified enum type.
+    ///     Thrown when the string value does not match any field in the specified enum type.
     /// </exception>
     public override Enum Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
