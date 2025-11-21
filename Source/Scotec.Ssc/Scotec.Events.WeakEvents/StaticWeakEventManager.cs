5d0c2ec554091530599cb2be59c6fbe0f5b6df76
Olaf Meyer <olaf.meyer@scotec-software.com>
2025-11-21 16:41:20 +0100

    Add support for weak handlers with EventArgs type
    Updated `Scotec.Events.WeakEvents.csproj` to include `StaticWeakEventManager.cs` in the compilation process by removing its exclusion and non-compilable resource entry.
    
    Added two new methods to `StaticWeakEventManager.cs`:
    - `AddWeakHandler<TObject>(TObject source, string eventName, Action<TObject, EventArgs> handler)` for adding weak event handlers with `EventArgs`.
    - `RemoveWeakHandler<TObject>(TObject source, string eventName, Action<TObject, EventArgs> handler)` for removing weak event handlers with `EventArgs`.
    
    These methods extend the existing functionality to handle events with the base `EventArgs` type.

--- Source/Scotec.Ssc/Scotec.Events.WeakEvents/StaticWeakEventManager.cs
+++ Source/Scotec.Ssc/Scotec.Events.WeakEvents/StaticWeakEventManager.cs
@@ -4,11 +4,23 @@ public static class StaticWeakEventManager
 {
     private static readonly WeakEventManager Instance = new();
 
+    public static void AddWeakHandler<TObject>(TObject source, string eventName, Action<TObject, EventArgs> handler)
+        where TObject : class
+    {
+        Instance.AddWeakHandler(source, eventName, handler);
+    }
+
     public static void AddWeakHandler<TObject, TEventArgs>(TObject source, string eventName, Action<TObject, TEventArgs> handler)
         where TObject : class
         where TEventArgs : EventArgs
     {
         Instance.AddWeakHandler(source, eventName, handler);
+    }
+
+    public static void RemoveWeakHandler<TObject>(TObject source, string eventName, Action<TObject, EventArgs> handler)
+        where TObject : class
+    {
+        Instance.RemoveWeakHandler(source, eventName, handler);
     }
 
     public static void RemoveWeakHandler<TObject, TEventArgs>(TObject source, string eventName, Action<TObject, TEventArgs> handler)
